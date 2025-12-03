f14aad1feac5de7aa645c8924198751abbf41edc
Ashish Soni <ashishsoni295work@gmail.com>
2025-12-03 20:04:01 +0530

    all components added; facing some error while predicting on localhost

--- src/configuration/mongo_db_connection.py
+++ src/configuration/mongo_db_connection.py
@@ -48,7 +48,7 @@ class MongoDBClient():
                 if mongo_db_url is None:
                     raise Exception(f"Environment variable '{MONGODB_URL_KEY}' is not set.")
                 
-            MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+                MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
 
             self.client = MongoDBClient.client
             self.database = self.client[databaseName]
